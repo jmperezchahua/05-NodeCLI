1a186a0c1da7d1cde41b7eb05f2c592e4147082d
jackelin perez <jmperezchahua@gmail.com>
2021-05-12 14:02:24 -0500

    commands done

--- bash.js
+++ bash.js
@@ -1,9 +1,9 @@
 const commands = require("./commands");
 
 // Output un prompt
+console.log("Ingresa algo...");
 process.stdout.write("prompt > ");
 // El evento stdin 'data' se dispara cuando el user escribe una línea
-console.log("Ingresa algo...");
 
 process.stdin.on("data", function (data) {
   const cmd = data.toString().trim(); // remueve la nueva línea
--- commands.js
+++ commands.js
@@ -2,13 +2,6 @@ const fs = require("fs");
 const readline = require("readline");
 
 const ls = (dir = ".") => {
-  //   fs.readdir(".", function (err, files) {
-  //     if (err) throw err;
-  //     files.forEach((file) => process.stdout.write(file.toString() + "\n"));
-  //     process.stdout.write("prompt > ");
-  //   });
-
-  /*Usando "readdirSync" */
   filenames = fs.readdirSync(dir);
   filenames.forEach((file) => {
     process.stdout.write(file.toString() + "\n");
@@ -16,21 +9,54 @@ const ls = (dir = ".") => {
 };
 
 const readLineFile = (file, cmd) => {
-  //Iniciando readFile
+  let data = {
+    lines: [],
+    linesNumber: 0,
+  };
+  const stream = fs.createReadStream(file);
+
+  stream.on("error", (err) => {
+    process.stdout.write(err + "\n");
+    process.stdout.write("prompt > ");
+  });
+
   const readInterface = readline.createInterface({
-    input: fs.createReadStream(file),
-    output: process.stdout,
+    input: stream,
   });
 
-  let count = 0;
-  readInterface.on("line", function (line) {
-    count++;
-    if (cmd === "head") {
-      if (count >= 5) readInterface.close();
-    }
-  });
+  readInterface
+    .on("line", (line) => {
+      data.lines.push(line);
+      data.linesNumber++;
 
-  // readInterface.on("history")
+      if (cmd === "head") {
+        if (data.linesNumber <= 5) {
+          process.stdout.write(line + "\n");
+        }
+      } else if (cmd === "cat") {
+        process.stdout.write(line + "\n");
+      }
+    })
+    .on("close", () => {
+      if (cmd === "tail") {
+        const dataLong = data.lines.length;
+        for (let index = dataLong - 5; index < dataLong; index++) {
+          process.stdout.write(data.lines[index] + "\n");
+        }
+      } else if (cmd === "sort") {
+        const newData = [...data.lines].sort();
+        newData.forEach((ele) => process.stdout.write(ele + "\n"));
+      } else if (cmd === "wc") {
+        process.stdout.write(data.linesNumber + "\n");
+      } else if (cmd === "uniq") {
+        const newData = [...data.lines].filter((ele, index, array) => {
+          return array.indexOf(ele) === index;
+        });
+        newData.forEach((ele) => process.stdout.write(ele + "\n"));
+      }
+
+      process.stdout.write("prompt > ");
+    });
 };
 
 const commands = ([...input]) => {
@@ -38,19 +64,23 @@ const commands = ([...input]) => {
 
   switch (input[0]) {
     case "pwd":
-      process.stdout.write(process.argv[1]);
+      process.stdout.write(process.argv[1] + "\n");
+      process.stdout.write("prompt > ");
       break;
     case "date":
-      process.stdout.write(Date());
+      process.stdout.write(Date() + "\n");
+      process.stdout.write("prompt > ");
       break;
     case "ls":
       ls();
+      process.stdout.write("prompt > ");
       break;
     case "echo":
-      process.stdout.write(nextCommand.join(" "));
+      process.stdout.write(nextCommand.join(" ") + "\n");
+      process.stdout.write("prompt > ");
       break;
     case "cat":
-      readLineFile(nextCommand[0]);
+      readLineFile(nextCommand[0], "cat");
       break;
     case "head":
       readLineFile(nextCommand[0], "head");
@@ -58,8 +88,18 @@ const commands = ([...input]) => {
     case "tail":
       readLineFile(nextCommand[0], "tail");
       break;
+    case "sort":
+      readLineFile(nextCommand[0], "sort");
+      break;
+    case "wc":
+      readLineFile(nextCommand[0], "wc");
+      break;
+    case "uniq":
+      readLineFile(nextCommand[0], "uniq");
+      break;
     default:
-      console.log("Ups! vuelve a escribir...");
+      process.stdout.write("Comando no reconocido :(" + "\n");
+      process.stdout.write("prompt > ");
   }
 };
 
